b0e87441189f9f6b176605a6f0ec8da616e51bc8
zkac269 <zkac269@live.rhul.ac.uk>
2022-11-11 20:30:23 +0000

    This is the first commit for the new feature branch NumStack. This branch is where NumStack will be developed and this first commit includes the first TDD test that tests the creation of a NumStack object. To get this test to work i had to create a NumStack class which is empty for now.

---
+++ src/test/java/calculator/NumStack.java
@@ -0,0 +1,10 @@
+package calculator;
+
+/**
+ * This is a stack of numeric values to hide the details of the stack class being used.
+ * 
+ * @author ZKAC269
+ */
+public class NumStack {
+
+}
---
+++ src/test/java/calculator/NumStackTest.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NumStackTest {
+  private NumStack NumStack;
+
+  @Test
+  void setup() {
+    NumStack = new NumStack();
+
+  }
+
+}
