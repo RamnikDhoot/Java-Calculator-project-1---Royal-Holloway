ac28a95a377fb952c77d9c89ed01b4f7d50a5fd8
zkac269 <zkac269@live.rhul.ac.uk>
2022-11-22 09:37:04 +0000

    This is the first commit for the new feature branch OpStack. This branch is where OpStack will be developed and this commit includes the first TDD test that tests the creation of an OpStack object. To get this test to pass I had to create a OpStack class which is empty for now.

---
+++ src/test/java/calculator/OpStackTest.java
@@ -0,0 +1,16 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OpStackTest {
+  private OpStack opStack;
+
+  @Test
+  void setup() {
+    opStack = new OpStack();
+
+  }
+
+}
