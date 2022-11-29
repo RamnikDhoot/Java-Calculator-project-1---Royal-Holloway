667ac7334665e1a6503e4c2f65d2e42def25a5ec
zkac269 <zkac269@live.rhul.ac.uk>
2022-11-29 21:35:41 +0000

    This is the first commit for the new feature branch StrStack. This branch is where StrStack will be developed and this first commit includes the first TDD test that tests the creation of a StrStack object. To get this test to work i had to create a StrStack class which is empty for now.

---
+++ src/test/java/calculator/StrStackTest.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class StrStackTest {
+
+  @Test
+  void setup() {
+    StrStack strStack = new StrStack();
+  }
+
+}
