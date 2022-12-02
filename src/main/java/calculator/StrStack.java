43ba216b22a5dbe6a66446f214d82d2ba14520a7
zkac269 <zkac269@live.rhul.ac.uk>
2022-12-02 14:25:49 +0000

    This commit adds a test for the size of the StrStack. I have made this test pass by adding a constructor, then adding a size method then faking the test by making it always return 0. I have also made a StrStack object be made before every tets using @BeforeEach.

--- src/main/java/calculator/StrStack.java
+++ src/main/java/calculator/StrStack.java
@@ -1,5 +1,27 @@
 package calculator;
 
+/**
+ * This is a stack of strings to be a facade for the underlying stack class.
+ * 
+ * @author ZKAC269
+ */
 public class StrStack {
+  Stack newStack;
+
+  /**
+   * constructor for the StrStack object.
+   */
+  public StrStack() {
+    newStack = new Stack();
+  }
+  
+  /**
+   * Returns the size of the object by calling size on the stack object.
+   * 
+   * @return size of the stack
+   */
+  public int size() {
+    return newStack.size();
+  }
 
 }
--- src/test/java/calculator/StrStackTest.java
+++ src/test/java/calculator/StrStackTest.java
@@ -1,13 +1,21 @@
 package calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StrStackTest {
+  private StrStack strStack;
 
-  @Test
+  @BeforeEach
   void setup() {
     StrStack strStack = new StrStack();
+  }
+
+  @Test
+  void testSize() {
+    assertEquals(strStack.size(), 0, "Tests new stack to see if its size is zero");
   }
 
 }
