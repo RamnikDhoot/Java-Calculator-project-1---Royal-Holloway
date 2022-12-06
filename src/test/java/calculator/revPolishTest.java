b2aad3cf74fa43da91477d374e8d55fc5a3f48b9
zkac269 <zkac269@live.rhul.ac.uk>
2022-12-06 16:31:55 +0000

    This is the first commit in the revPolish feature branch. I have firstly added a test that creates a RevPolishobject and made it pass by creating the RevPolish class which is empty.

---
+++ src/test/java/calculator/revPolishTest.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class revPolishTest {
+
+  @Test
+  void test() {
+    RevPolish calc = new RevPolish();
+  }
+
+}
