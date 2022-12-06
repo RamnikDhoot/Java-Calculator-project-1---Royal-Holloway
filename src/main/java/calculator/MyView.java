a8d31120b8b25f662a2beb414989a852edb051bc
zkac269 <zkac269@live.rhul.ac.uk>
2022-12-06 10:05:48 +0000

    Added more css for the styling of the gui and changed the gui to inclue all the nessessary functions including radio buttons and text fields. Also populated the MyView class with the skeleton code from scene builder.

--- src/main/java/calculator/MyView.java
+++ src/main/java/calculator/MyView.java
@@ -1,5 +1,34 @@
 package calculator;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class MyView {
 
+  @FXML
+  private Label Expression;
+
+  @FXML
+  private TextField FieldE;
+
+  @FXML
+  private TextField FieldR;
+
+  @FXML
+  private RadioButton Infix;
+
+  @FXML
+  private Label Result;
+
+  @FXML
+  private RadioButton ReversePolish;
+
+  @FXML
+  private Button calculate;
+
 }
+
+
