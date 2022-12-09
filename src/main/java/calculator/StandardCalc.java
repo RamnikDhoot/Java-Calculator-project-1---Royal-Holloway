27cdbc2e630c783c6041f410e9f06669d9c199d1
zkac269 <zkac269@live.rhul.ac.uk>
2022-12-09 13:43:25 +0000

    Test for evaluate method, to make it pass i added the evaluate method to StandardCalc and made it always return 0.

--- src/main/java/calculator/StandardCalc.java
+++ src/main/java/calculator/StandardCalc.java
@@ -1,5 +1,13 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class StandardCalc {
+  
+  private OpStack list;
+  
+  public Float evaluate(String str) {
+    return (float) 0;
+  }
 
 }
--- src/test/java/calculator/StandardCalcTest.java
+++ src/test/java/calculator/StandardCalcTest.java
@@ -1,13 +1,21 @@
 package calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StandardCalcTest {
+  private StandardCalc calc;
 
+  @BeforeEach
+  void setup() {
+    calc = new StandardCalc();
+  }
+  
   @Test
-  void test() {
-    StandardCalc calc = new StandardCalc();
+  void testList() throws BadTypeException {
+    assertEquals(calc.evaluate("test"), 0);
   }
 
 }
